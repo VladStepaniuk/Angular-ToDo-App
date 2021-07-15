9ed0f5a1cc63b5ff41389ee516732c3aefac5de7
Vlad <48446407+VladStepaniuk@users.noreply.github.com>
2021-07-15 22:21:35 +0300

    Created todo edit component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ToDo } from '../shared/todo.model';
 import { DataService } from '../shared/data.service';
 import { NgForm } from '@angular/forms';
+import { EditTodoDialogComponent } from '../edit-todo-dialog/edit-todo-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-todos',
@@ -13,7 +15,7 @@ export class TodosComponent implements OnInit {
   todos: ToDo[]
   showValidationErrors: boolean
 
-  constructor(private dataService: DataService/*, private dialog: MatDialog*/) { }
+  constructor(private dataService: DataService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.todos = this.dataService.getAllTodos()
@@ -32,7 +34,7 @@ export class TodosComponent implements OnInit {
     todo.completed = !todo.completed;
   }
 
-  /*editTodo(todo: ToDo) {
+  editTodo(todo: ToDo) {
     const index = this.todos.indexOf(todo)
 
     let dialogRef = this.dialog.open(EditTodoDialogComponent, {
@@ -45,7 +47,7 @@ export class TodosComponent implements OnInit {
         this.dataService.updateTodo(index, result)
       }
     })
-  }*/
+  }
 
   deleteTodo(todo: ToDo) {
     const index = this.todos.indexOf(todo)
